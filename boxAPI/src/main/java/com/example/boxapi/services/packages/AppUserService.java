629a50d8546b3830123238387a6c9f1035f3d120
thea.rime <thea.rime@hypatia.no>
2022-10-04 14:39:52 +0200

    Created Services.

---
+++ boxAPI/src/main/java/com/example/boxapi/services/packages/AppUserService.java
@@ -0,0 +1,7 @@
+package com.example.boxapi.services.packages;
+
+import com.example.boxapi.models.AppUser;
+import com.example.boxapi.services.CrudService;
+
+public interface AppUserService extends CrudService<AppUser, Integer> {
+}
---
+++ boxAPI/src/main/java/com/example/boxapi/services/packages/CountryService.java
@@ -0,0 +1,7 @@
+package com.example.boxapi.services.packages;
+
+import com.example.boxapi.models.Country;
+import com.example.boxapi.services.CrudService;
+
+public interface CountryService extends CrudService<Country, Integer> {
+}
