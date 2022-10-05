7790ee39e9d9cde6f383dce6aa59ec8c21efdafd
thea.rime <thea.rime@hypatia.no>
2022-10-05 12:41:35 +0200

    added dependencies.

--- boxAPI/src/main/java/com/example/boxapi/services/appuser/AppUserServiceImpl.java
+++ boxAPI/src/main/java/com/example/boxapi/services/appuser/AppUserServiceImpl.java
@@ -1,15 +1,23 @@
 package com.example.boxapi.services.appuser;
 
 import com.example.boxapi.models.AppUser;
+import com.example.boxapi.repositories.AppUserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
 @Service
 public class AppUserServiceImpl implements AppUserService{
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserServiceImpl(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
     @Override
-    public AppUser findById(Integer integer) {
-        return null;
+    public AppUser findById(Integer id) {
+        return (AppUser) appUserRepository.findById(id).get();
     }
 
     @Override
