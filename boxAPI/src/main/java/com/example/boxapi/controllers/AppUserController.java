09a3f89ac5dff9804d34bc67ddded78337b7d419
thea.rime <thea.rime@hypatia.no>
2022-10-10 13:27:30 +0200

    added update to controller.

--- boxAPI/src/main/java/com/example/boxapi/controllers/AppUserController.java
+++ boxAPI/src/main/java/com/example/boxapi/controllers/AppUserController.java
@@ -4,15 +4,14 @@ package com.example.boxapi.controllers;
 import com.example.boxapi.mappers.AppUserMapper;
 import com.example.boxapi.models.AppUser;
 import com.example.boxapi.models.dto.AppUserDTO;
+import com.example.boxapi.models.dto.CountryDTO;
 import com.example.boxapi.services.appuser.AppUserServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,24 +56,15 @@ public class AppUserController {
 
         return ResponseEntity.ok(appUsers);
     }
-/*
-    @PostMapping("/save")
-    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user) {
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/account/save").toUriString());
-        return ResponseEntity.created(uri).body(appUserService.saveUser(user));
-    }
 
- */
-
-
-    @Operation(summary = "Add user")
+    @Operation(summary = "Add new user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201",
                     description = "Created",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = AppUserDTO.class))}),
             @ApiResponse(responseCode = "404",
-                    description = "Movie cannot be created",
+                    description = "User cannot be created",
                     content = @Content)
     })
     @PostMapping()
@@ -86,24 +76,29 @@ public class AppUserController {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Update existing user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CountryDTO.class))}),
+            @ApiResponse(responseCode = "404",
+                    description = "User not found with supplied ID",
+                    content = @Content)
+    })
+    @PutMapping (":{id}") // GET: localhost:8080/api/v1/settings/countries:1
+    public ResponseEntity update(@RequestBody AppUserDTO appUserDTO, @PathVariable int id) {
+        if (appUserDTO.getUser_id() != id) {
+            ResponseEntity.badRequest().build();
+        }
+        AppUser updatedUser = appUserService.update(
+                appUserMapper.appUserDTOtoAppUser(appUserDTO)
+        );
 
-
-
-    /*PostMapping("/role/save")
-    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/role/save").toUriString());
-        return ResponseEntity.created(uri).body(appUserService.saveRole(role));
+        return ResponseEntity.noContent().build();
     }
 
-     */
 
-    /*PostMapping("/role/addtouser")
-    public ResponseEntity<?> saveRole(@RequestBody RoleToUserForm form) {
-        appUserService.addRoleToUser(form.getUsername(), form.getRoleName());
-        return ResponseEntity.ok().build();
-    }
-
-     */
 
 
     // This endpoint just shows the information from the token
