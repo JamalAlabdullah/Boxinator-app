6c340547bf5f02d98ff06f196c00f3728af4454e
thea.rime <thea.rime@hypatia.no>
2022-10-17 14:29:01 +0200

    changed weight to getAll instead of byId

--- boxAPI/src/main/java/com/example/boxapi/controllers/WeightController.java
+++ boxAPI/src/main/java/com/example/boxapi/controllers/WeightController.java
@@ -2,11 +2,12 @@ package com.example.boxapi.controllers;
 
 import com.example.boxapi.mappers.WeightMapper;
 import com.example.boxapi.models.dto.WeightDTO;
-import com.example.boxapi.models.enums.WeightType;
 import com.example.boxapi.services.weight.WeightService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -16,13 +17,11 @@ public class WeightController {
 
     private final WeightMapper weightMapper;
     private final WeightService weightService;
-    @GetMapping("{id}") // GET: localhost:8080/api/v1/account/1
-    //RolesAllowed("user") //case sensitive!
-    public ResponseEntity getById(@PathVariable WeightType id) {
-        WeightDTO weightDTO = weightMapper.weightToWeightDTO(weightService.findById(id));
+
+    @GetMapping
+    public ResponseEntity<Collection<WeightDTO>> get() {
+        Collection<WeightDTO> weightDTO = weightMapper.weightToWeightDTO(weightService.findAll());
         return ResponseEntity.ok(weightDTO);
     }
-    //    AppUserDTO appUserDTO = appUserMapper.appUserToAppUserDTO(appUserService.findById(id));
-    //    return ResponseEntity.ok(appUserDTO);
-    //}
+
 }
